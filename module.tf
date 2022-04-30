35e01183cc89cb258ffae01fabdc47077b52cdde
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-04-30 06:10:34 -0500

    Create module.tf

---
+++ module.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "panorama_sg"
+  description = "Basic communication for Panorama"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
